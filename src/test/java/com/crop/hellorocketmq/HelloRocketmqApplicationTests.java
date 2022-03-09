e9c6d9bacf21343fbf2e58799bb52a9cb69ca1a5
linmeng <1812323571@qq.com>
2022-03-09 10:25:36 +0800

    init

---
+++ src/test/java/com/crop/hellorocketmq/HelloRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crop.hellorocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloRocketmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
